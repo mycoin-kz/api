326b7f5b0a67779cc13747ad0a7bb757945f148e
zhassbala <besimbekovr@gmail.com>
2022-10-24 04:55:38 +0600

    upd

--- main_module/views.py
+++ main_module/views.py
@@ -22,12 +22,16 @@ def watchlist(request):
         try: 
             watchlist_obj = Watchlist(user=user, token=request.data['token'])
             watchlist_obj.save()
-            serializer = WatchlistSerializer(watchlist_obj)
-            return Response(serializer.data)
         except IntegrityError:
-            return Response(data='Token already exists in user\'s watchlist', status=status.HTTP_400_BAD_REQUEST)
+            pass
+        serializer = WatchlistSerializer(user.watchlist, many=True)
+        return Response(serializer.data)
     if request.method == 'DELETE':
         user = check_auth(request)
-        watchlist = Watchlist.objects.get(user=user, token=request.data['token'])
-        watchlist.delete()
-        return Response(status=status.HTTP_200_OK)
+        try:
+            watchlist = Watchlist.objects.get(user=user, token=request.data['token'])
+            watchlist.delete()
+        except:
+            pass
+        serializer = WatchlistSerializer(user.watchlist, many=True)
+        return Response(serializer.data)

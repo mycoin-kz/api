8ca4652ee3dbb678c55d2e9b224fabc582aa7f27
Rassul Bessimbekov <besimbekovr@gmail.com>
2023-12-18 01:05:13 +0600

    feat: update token model

--- main_module/models.py
+++ main_module/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from auth_module.models import User
 
@@ -25,6 +26,38 @@ class Token(models.Model):
     cryptocompare_assetlaunchdate = models.CharField(max_length=255)
     cryptocompare_assetwebsiteurl = models.CharField(max_length=255)
     coin_status = models.CharField(max_length=255)
+
+    codrepo_perc = models.FloatField(
+        validators=[MinValueValidator(0.0), MaxValueValidator(100.0)],
+        null=True,
+        blank=True,
+    )
+    fb_perc = models.FloatField(
+        validators=[MinValueValidator(0.0), MaxValueValidator(100.0)],
+        null=True,
+        blank=True,
+    )
+    reddit_perc = models.FloatField(
+        validators=[MinValueValidator(0.0), MaxValueValidator(100.0)],
+        null=True,
+        blank=True,
+    )
+    twitter_perc = models.FloatField(
+        validators=[MinValueValidator(0.0), MaxValueValidator(100.0)],
+        null=True,
+        blank=True,
+    )
+    imageurl = models.CharField(max_length=512, null=True, blank=True)
+    fullname = models.CharField(max_length=255, null=True, blank=True)
+    symbol = models.CharField(max_length=128, null=True, blank=True)
+    total_perc = models.FloatField(
+        validators=[MinValueValidator(0.0), MaxValueValidator(100.0)],
+        null=True,
+        blank=True,
+    )
+    bullish = models.IntegerField(null=True, blank=True)
+    neutral = models.IntegerField(null=True, blank=True)
+    bearish = models.IntegerField(null=True, blank=True)
 
 
 class TokenOHLCV(models.Model):

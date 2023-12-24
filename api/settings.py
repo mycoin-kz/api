dd456602d3dc3f5c7bd866a9e3613ae92ad4ab9b
Rassul Bessimbekov <besimbekovr@gmail.com>
2023-12-24 18:03:23 +0600

    feat: change python version (temp)

--- api/settings.py
+++ api/settings.py
@@ -150,10 +150,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "auth_module.User"
 
-CORS_ALLOWED_ORIGINS = config(
-    "FRONTEND_URL", "https://localhost:8000 https://localhost:8080"
-).split(" ")
-# CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = config(
+#     "FRONTEND_URL", "https://localhost:8000 https://localhost:8080"
+# ).split(" ")
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 CSRF_COOKIE_PATH = "/"
 CSRF_COOKIE_SAMESITE = "None"
